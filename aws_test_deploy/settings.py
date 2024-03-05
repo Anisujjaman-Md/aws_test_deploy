4c7de70c39f782eb6307430130e6e4c8dfd8ad44
Anisujjaman-Md <Contact.anisujjaman@gmail.com>
2024-03-05 18:28:14 +0600

    Docker Compose YAML change

--- aws_test_deploy/settings.py
+++ aws_test_deploy/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6378/0'
 CELERY_RESULT_BACKEND = 'rpc://'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
