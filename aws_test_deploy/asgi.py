a92c60e09761f44dff379a8a28637f53b7a18c7f
Anisujjaman-Md <Contact.anisujjaman@gmail.com>
2024-03-09 17:56:25 +0600

    web socket config

--- aws_test_deploy/asgi.py
+++ aws_test_deploy/asgi.py
@@ -1,19 +1,14 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from notifaction_test import routing
 from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aws_test_deploy.settings')
 
-from notifaction_test.routing import websocket_urlpatterns
-
 application = ProtocolTypeRouter(
     {
-        'http': get_asgi_application(),
-        'websocket': AuthMiddlewareStack(
-            URLRouter(
-                websocket_urlpatterns
-            )
-        ),
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
     }
 )
--- aws_test_deploy/urls.py
+++ aws_test_deploy/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 from  django.conf.urls.static import static
 
@@ -23,8 +23,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("test_app.urls")),
     path('user/', include("user.urls")),
-
+    # re_path("ws/", include("notifaction_test.routing")),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
